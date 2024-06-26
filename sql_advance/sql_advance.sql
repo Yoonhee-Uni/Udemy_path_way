a5ec2ae716cea947aa8dc30c5548d74fd5888e9f
Yoonhee-Uni <yoonheeleeuk@gmail.com>
2024-06-26 22:20:30 +0100

    updated ROW_NUMBER, RANK(), DENSE_RANK()

--- sql_advance/sql_advance.sql
+++ sql_advance/sql_advance.sql
@@ -126,6 +126,135 @@ ON P.ProductSubcategoryID = PP.ProductSubcategoryID
 JOIN Production.ProductCategory PPC
 ON PP.ProductCategoryID = PPC.ProductCategoryID;
 
+--ROW_NUMBER()
+SELECT
+SalesOrderID,
+SalesOrderDetailID,
+LineTotal,
+ProductIDLineTotal = SUM(LineTotal) OVER (PARTITION BY SalesOrderID),
+Ranking = ROW_NUMBER() OVER (PARTITION BY SalesOrderID ORDER BY LineTotal DESC)
+FROM Sales.SalesOrderDetail 
+ORDER BY SalesOrderID;
+
+SELECT
+SalesOrderID,
+SalesOrderDetailID,
+LineTotal,
+ProductIDLineTotal = SUM(LineTotal) OVER (PARTITION BY SalesOrderID),
+Ranking = ROW_NUMBER() OVER (ORDER BY LineTotal DESC)
+FROM Sales.SalesOrderDetail 
+ORDER BY 5;
 
 
+-- Exercise 2
+SELECT 
+P.Name AS ProductName , 
+P.ListPrice, 
+PP.Name AS ProductSubcategory,
+PPC.Name AS ProductCategory,
+ROW_NUMBER() OVER(ORDER BY P.ListPrice DESC) AS PriceRank
+
+FROM Production.Product P
+JOIN Production.ProductSubcategory PP
+ON P.ProductSubcategoryID = PP.ProductSubcategoryID
+JOIN Production.ProductCategory PPC
+ON PP.ProductCategoryID = PPC.ProductCategoryID
+ORDER BY 5;
+
+-- Exercise 3
+SELECT 
+P.Name AS ProductName , 
+P.ListPrice, 
+PP.Name AS ProductSubcategory,
+PPC.Name AS ProductCategory,
+ROW_NUMBER() OVER(ORDER BY P.ListPrice DESC) AS PriceRank,
+ROW_NUMBER() OVER (PARTITION BY PPC.Name ORDER BY P.ListPrice DESC) AS Category_Price_Rank
+
+FROM Production.Product P
+JOIN Production.ProductSubcategory PP
+ON P.ProductSubcategoryID = PP.ProductSubcategoryID
+JOIN Production.ProductCategory PPC
+ON PP.ProductCategoryID = PPC.ProductCategoryID
+ORDER BY 5;
+
+-- Exercise 4
+SELECT 
+P.Name AS ProductName , 
+P.ListPrice, 
+PP.Name AS ProductSubcategory,
+PPC.Name AS ProductCategory,
+ROW_NUMBER() OVER(ORDER BY P.ListPrice DESC) AS PriceRank,
+ROW_NUMBER() OVER (PARTITION BY PPC.Name ORDER BY P.ListPrice DESC) AS Category_Price_Rank,
+
+CASE
+    WHEN ROW_NUMBER() OVER (PARTITION BY PPC.Name ORDER BY P.ListPrice DESC) <= 5 THEN 'Yes'
+    ELSE 'No'
+END AS Top_5_Price_In_Category
+
+FROM Production.Product P
+JOIN Production.ProductSubcategory PP
+ON P.ProductSubcategoryID = PP.ProductSubcategoryID
+JOIN Production.ProductCategory PPC
+ON PP.ProductCategoryID = PPC.ProductCategoryID
+ORDER BY 5;
+
+
+--Rank()
+SELECT
+SalesOrderID,
+SalesOrderDetailID,
+LineTotal,
+Ranking = ROW_NUMBER() OVER (PARTITION BY SalesOrderID ORDER BY LineTotal DESC),
+RankingWithRank = RANK() OVER (PARTITION BY SalesOrderID ORDER BY LineTotal DESC),
+RankingWithDense_Rank = DENSE_RANK() OVER (PARTITION BY SalesOrderID ORDER BY LineTotal DESC)
+FROM Sales.SalesOrderDetail 
+;
+
+-- RANK and DENSE_RANK - Exercises
+-- Exercise 1
+SELECT 
+P.Name AS ProductName , 
+P.ListPrice, 
+PP.Name AS ProductSubcategory,
+PPC.Name AS ProductCategory,
+ROW_NUMBER() OVER(ORDER BY P.ListPrice DESC) AS PriceRank,
+ROW_NUMBER() OVER (PARTITION BY PPC.Name ORDER BY P.ListPrice DESC) AS Category_Price_Rank,
+CASE
+    WHEN ROW_NUMBER() OVER (PARTITION BY PPC.Name ORDER BY P.ListPrice DESC) <= 5 THEN 'Yes'
+    ELSE 'No'
+END AS Top_5_Price_In_Category,
+
+RANK() OVER(PARTITION BY PPC.Name ORDER BY P.ListPrice DESC) AS Category_Price_Rank_With_Rank
+
+FROM Production.Product P
+JOIN Production.ProductSubcategory PP
+ON P.ProductSubcategoryID = PP.ProductSubcategoryID
+JOIN Production.ProductCategory PPC
+ON PP.ProductCategoryID = PPC.ProductCategoryID
+;
+
+-- Exercise 2
+SELECT 
+P.Name AS ProductName , 
+P.ListPrice, 
+PP.Name AS ProductSubcategory,
+PPC.Name AS ProductCategory,
+ROW_NUMBER() OVER(ORDER BY P.ListPrice DESC) AS PriceRank,
+ROW_NUMBER() OVER (PARTITION BY PPC.Name ORDER BY P.ListPrice DESC) AS Category_Price_Rank,
+CASE
+    WHEN ROW_NUMBER() OVER (PARTITION BY PPC.Name ORDER BY P.ListPrice DESC) <= 5 THEN 'Yes'
+    ELSE 'No'
+END AS Top_5_Price_In_Category,
+
+RANK() OVER(PARTITION BY PPC.Name ORDER BY P.ListPrice DESC) AS Category_Price_Rank_With_Rank,
+DENSE_RANK() OVER(PARTITION BY PPC.Name ORDER BY P.ListPrice DESC) AS Category_Price_Rank_With_Dense_Rank
+
+FROM Production.Product P
+JOIN Production.ProductSubcategory PP
+ON P.ProductSubcategoryID = PP.ProductSubcategoryID
+JOIN Production.ProductCategory PPC
+ON PP.ProductCategoryID = PPC.ProductCategoryID
+;
+
+-- Exercise 3
 
