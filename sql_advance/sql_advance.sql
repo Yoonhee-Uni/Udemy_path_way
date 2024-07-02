17a4c03e0655226dbad25a8ceac3f8f5b82f3fc4
Yoonhee-Uni <yoonheeleeuk@gmail.com>
2024-07-02 22:15:35 +0100

    updated on Subqueries, corrlated subqueries, XNK PATH, PIVOT

--- sql_advance/sql_advance.sql
+++ sql_advance/sql_advance.sql
@@ -480,27 +480,487 @@ FROM Purchasing.PurchaseOrderHeader
 ) A
 WHERE Most_expensive BETWEEN 1 AND 3;
 
+--ROWS BETWEEN
+
+-- Early 3days of sum INCLUDING CURRENT ROW
 SELECT
-	PurchaseOrderID,
-	VendorID,
-	OrderDate,
-	TaxAmt,
-	Freight,
-	TotalDue
+OrderDate,
+TotalDue,
+SalesLast3Days = SUM(TotalDue) OVER(ORDER BY OrderDate ROWS BETWEEN 2 PRECEDING AND CURRENT ROW)
 
-FROM (
-	SELECT 
-		PurchaseOrderID,
-		VendorID,
-		OrderDate,
-		TaxAmt,
-		Freight,
-		TotalDue,
-		PurchaseOrderRank = DENSE_RANK() OVER(PARTITION BY VendorID ORDER BY TotalDue DESC)
-
-	FROM AdventureWorks2019.Purchasing.PurchaseOrderHeader
+FROM(
+SELECT
+OrderDate,
+TotalDue = SUM(TotalDue)
+FROM
+Sales.SalesOrderHeader
+WHERE YEAR(OrderDate) = 2014
+GROUP BY OrderDate
 ) X
 
-WHERE PurchaseOrderRank <= 3
+-- Early 3days of sum EXCLUDING CURRENT ROW
+SELECT
+OrderDate,
+TotalDue,
+SalesLast3Days = SUM(TotalDue) OVER(ORDER BY OrderDate ROWS BETWEEN 3 PRECEDING AND 1 PRECEDING)
+
+FROM(
+SELECT
+OrderDate,
+TotalDue = SUM(TotalDue)
+FROM
+Sales.SalesOrderHeader
+WHERE YEAR(OrderDate) = 2014
+GROUP BY OrderDate
+) X;
+
+--Span the current row and include the row prior and the row after.
+-- Rolling 3day total, spanning previous and following row.
+SELECT
+OrderDate,
+TotalDue,
+SalesLast3Days = SUM(TotalDue) OVER(ORDER BY OrderDate ROWS BETWEEN 1 PRECEDING AND 1 FOLLOWING) 
+
+FROM(
+SELECT
+OrderDate,
+TotalDue = SUM(TotalDue)
+FROM
+Sales.SalesOrderHeader
+WHERE YEAR(OrderDate) = 2014
+GROUP BY OrderDate
+) X;
 
 
+-- Rolling 3 day average -aka, a "moving" average
+SELECT
+OrderDate,
+TotalDue,
+SalesLast3Days = AVG(TotalDue) OVER(ORDER BY OrderDate ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) 
+
+FROM(
+SELECT
+OrderDate,
+TotalDue = SUM(TotalDue)
+FROM
+Sales.SalesOrderHeader
+WHERE YEAR(OrderDate) = 2014
+GROUP BY OrderDate
+) X;
+
+
+-- ROWS BETWEEN - Exercises
+-- Exercise 1
+
+SELECT 
+OrderMonth = MONTH(OrderDate),
+OrderYear = YEAR(OrderDate),
+SubTotal = SUM(TotalDue)
+
+FROM
+Sales.SalesOrderHeader
+GROUP BY OrderDate;
+
+-- Exercise 2
+
+SELECT
+OrderMonth,
+OrderYear,
+SubTotal,
+Rolling3MonthTotal = SUM(SubTotal) OVER(ORDER BY OrderMonth, OrderYear ROWS BETWEEN 2 PRECEDING AND CURRENT ROW)
+
+FROM
+(SELECT 
+OrderMonth = MONTH(OrderDate),
+OrderYear = YEAR(OrderDate),
+SubTotal = SUM(SubTotal)
+
+FROM
+Sales.SalesOrderHeader
+GROUP BY OrderDate
+) x;
+
+-- Exercise 3
+
+SELECT
+OrderMonth,
+OrderYear,
+SubTotal,
+Rolling3MonthTotal = SUM(SubTotal) OVER(ORDER BY OrderMonth, OrderYear ROWS BETWEEN 2 PRECEDING AND CURRENT ROW),
+MovingAvg6Month = AVG(SubTotal) OVER(ORDER BY OrderMonth, OrderYear ROWS BETWEEN 6 PRECEDING AND 1 PRECEDING)
+
+FROM
+(SELECT 
+OrderMonth = MONTH(OrderDate),
+OrderYear = YEAR(OrderDate),
+SubTotal = SUM(SubTotal)
+
+FROM
+Sales.SalesOrderHeader
+GROUP BY OrderDate
+) x;
+
+-- Exercise 4
+SELECT
+OrderMonth,
+OrderYear,
+SubTotal,
+Rolling3MonthTotal = SUM(SubTotal) OVER(ORDER BY OrderMonth, OrderYear ROWS BETWEEN 2 PRECEDING AND CURRENT ROW),
+MovingAvg6Month = AVG(SubTotal) OVER(ORDER BY OrderMonth, OrderYear ROWS BETWEEN 6 PRECEDING AND 1 PRECEDING),
+MovingAvgNext2Months = AVG(SubTotal) OVER(ORDER BY OrderMonth, OrderYear ROWS BETWEEN CURRENT ROW AND 2 FOLLOWING)
+
+FROM
+(SELECT 
+OrderMonth = MONTH(OrderDate),
+OrderYear = YEAR(OrderDate),
+SubTotal = SUM(SubTotal)
+
+FROM
+Sales.SalesOrderHeader
+GROUP BY OrderDate
+) x;
+
+-- Subqueries
+SELECT
+MAX(ListPrice)
+FROM Production.Product;
+
+SELECT 
+AVG(ListPrice)
+FROM Production.Product;
+
+SELECT
+ProductID,
+Name,
+StandardCost,
+ListPrice,
+AvgListPrice = (SELECT AVG(ListPrice) FROM Production.Product),
+AvgListPriceDiff = ListPrice - (SELECT AVG(ListPrice) FROM Production.Product)
+FROM Production.Product
+
+WHERE ListPrice > (SELECT AVG(ListPrice) FROM Production.Product)
+ORDER BY ListPrice;
+
+-- Scalar Subqueries - Exercises
+-- Exercise 1
+SELECT 
+BusinessEntityID,
+JobTitle,
+VacationHours,
+MaxVacationHours = (SELECT MAX(VacationHours) FROM HumanResources.Employee)
+
+
+FROM
+HumanResources.Employee;
+
+-- Exercise 2
+SELECT 
+BusinessEntityID,
+JobTitle,
+VacationHours,
+MaxVacationHours = (SELECT MAX(VacationHours) FROM HumanResources.Employee),
+PercentageOfVacationHours = (( VacationHours * 1.0 ) / (SELECT MAX(VacationHours) FROM HumanResources.Employee)*100)
+FROM
+HumanResources.Employee;
+
+-- Exercise 3
+SELECT 
+BusinessEntityID,
+JobTitle,
+VacationHours,
+MaxVacationHours = (SELECT MAX(VacationHours) FROM HumanResources.Employee),
+PercentageOfVacationHours = (( VacationHours * 1.0 ) / (SELECT MAX(VacationHours) FROM HumanResources.Employee)*100)
+FROM
+HumanResources.Employee
+
+WHERE (( VacationHours * 1.0 ) / (SELECT MAX(VacationHours) FROM HumanResources.Employee)*100)>  80;
+
+-- Correlated Subqueries - Exercises
+SELECT
+SalesOrderID,
+OrderDate,
+SubTotal,
+TaxAmt,
+Freight,
+TotalDue,
+MultiOrderCount = 
+    (
+    SELECT COUNT(*)
+    FROM Sales.SalesOrderDetail B
+    WHERE B.SalesOrderID = A.SalesOrderID
+    AND B.OrderQty > 1
+    )
+FROM Sales.SalesOrderHeader A;
+
+SELECT SalesOrderID,
+OrderQty
+FROM Sales.SalesOrderDetail
+WHERE SalesOrderID = 43660;
+
+-- Exercise 1
+SELECT
+PurchaseOrderID,
+VendorID,
+OrderDate,
+TotalDue,
+
+NonRejectedItems = 
+    (
+        SELECT COUNT(*)
+        FROM Purchasing.PurchaseOrderDetail PPD
+        WHERE PPD.PurchaseOrderID = PPO.PurchaseOrderID
+        AND
+        RejectedQty = 0
+    )
+FROM
+Purchasing.PurchaseOrderHeader PPO;
+
+-- SELECT PurchaseOrderID, RejectedQty
+-- FROM Purchasing.PurchaseOrderDetail PPD
+-- WHERE  RejectedQty > 0
+-- ORDER BY PurchaseOrderDetailID
+
+-- Exercise 2
+SELECT
+PurchaseOrderID,
+VendorID,
+OrderDate,
+TotalDue,
+
+NonRejectedItems = 
+    (
+        SELECT COUNT(*)
+        FROM Purchasing.PurchaseOrderDetail PPD
+        WHERE PPD.PurchaseOrderID = PPO.PurchaseOrderID
+        AND
+        RejectedQty = 0
+    ),
+
+MostExpensiveItem = 
+    (
+        SELECT MAX(UnitPrice)
+        FROM Purchasing.PurchaseOrderDetail PPD
+        WHERE PPD.PurchaseOrderID = PPO.PurchaseOrderID
+    )
+
+
+FROM
+Purchasing.PurchaseOrderHeader PPO;
+
+--EXISTS and NOT EXISTS
+--Using exists to pick only the records we need
+SELECT 
+A.SalesOrderID,
+A.OrderDate,
+A.TotalDue
+FROM Sales.SalesOrderHeader A
+WHERE EXISTS(
+    SELECT 
+    1
+    FROM Sales.SalesOrderDetail B
+    WHERE B.LineTotal > 10000
+    AND A.SalesOrderID = B.SalesOrderID
+    
+)
+
+ORDER BY 1
+
+-- Exclusionary one to many Join
+SELECT
+A.SalesOrderID,
+A.OrderDate,
+A.TotalDue,
+B.SalesOrderDetailID,
+B.LineTotal
+FROM Sales.SalesOrderHeader A
+INNER JOIN Sales.SalesOrderDetail B
+ON A.SalesOrderID = B.SalesOrderID
+WHERE B.LineTotal < 10000
+AND A.SalesOrderID = 43683
+ORDER BY 1;
+
+
+--Using exists to pick only the records we need
+SELECT 
+A.SalesOrderID,
+A.OrderDate,
+A.TotalDue
+FROM Sales.SalesOrderHeader A
+WHERE NOT EXISTS(
+    SELECT 
+    1
+    FROM Sales.SalesOrderDetail B
+    WHERE B.LineTotal > 10000
+    AND A.SalesOrderID = B.SalesOrderID
+    
+)
+
+-- EXISTS - Exercises
+-- Exercise 1
+SELECT
+    A.PurchaseOrderID,
+    A.OrderDate,
+    A.SubTotal,
+    A.TaxAmt
+FROM Purchasing.PurchaseOrderHeader A
+WHERE EXISTS (
+    SELECT 1
+    FROM Purchasing.PurchaseOrderDetail B
+    WHERE B.PurchaseOrderID = A.PurchaseOrderID
+    AND B.OrderQty  > 500
+)
+ORDER BY 1;
+
+-- Exercise 2
+SELECT
+    A.*
+FROM Purchasing.PurchaseOrderHeader A
+WHERE EXISTS (
+    SELECT 1
+    FROM Purchasing.PurchaseOrderDetail B
+    WHERE B.PurchaseOrderID = A.PurchaseOrderID
+    AND B.OrderQty  > 500
+    AND B.UnitPrice > 50
+)
+ORDER BY 1;
+
+-- Exercise 3
+SELECT
+    A.*
+FROM Purchasing.PurchaseOrderHeader A
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM Purchasing.PurchaseOrderDetail B
+    WHERE B.PurchaseOrderID = A.PurchaseOrderID
+    AND B.RejectedQty > 0
+
+)
+ORDER BY 1;
+
+SELECT * FROM Purchasing.PurchaseOrderDetail
+WHERE RejectedQty = 0
+
+--Flattning
+SELECT
+STUFF(
+    (
+    SELECT
+    ',' + CAST(CAST(LineTotal AS MONEY) AS VARCHAR)
+    FROM Sales.SalesOrderDetail A
+    WHERE A.SalesOrderID = 43659
+    FOR XML PATH('')
+    ),
+    1,1,'')
+
+SELECT
+SalesOrderID,
+OrderDate,
+SubTotal,
+TaxAmt,
+Freight,
+TotalDue,
+LineTotals = 
+STUFF(
+    (
+    SELECT
+    ',' + CAST(CAST(LineTotal AS MONEY) AS VARCHAR)
+    FROM Sales.SalesOrderDetail B
+    WHERE A.SalesOrderID = B.SalesOrderID
+    FOR XML PATH('')
+    ),
+    1,1,'')
+FROM Sales.SalesOrderHeader A
+
+-- Exercise 1
+SELECT
+SubcategoryName = 
+STUFF(
+    (
+    SELECT 
+    ';' + B.Name
+    FROM Production.Product B
+    WHERE A.ProductSubcategoryID = B.ProductSubcategoryID
+    FOR XML PATH('')
+    ),
+    1,1,''
+)
+FROM Production.ProductSubcategory A 
+
+-- Exercise 2
+SELECT
+SubcategoryName = 
+STUFF(
+    (
+    SELECT 
+    ';' + B.Name
+    FROM Production.Product B
+    WHERE A.ProductSubcategoryID = B.ProductSubcategoryID
+    AND  B.ListPrice > 50
+    FOR XML PATH('')
+    ),
+    1,1,''
+)
+FROM Production.ProductSubcategory A;
+
+--PIVOT
+SELECT
+[Bikes], [clothing], [Accessories], [Components]
+FROM
+
+(SELECT
+ProductCategoryName = D.NAME, 
+A.LineTotal
+
+FROM Sales.SalesOrderDetail A
+JOIN Production.Product B 
+ON A.ProductID =B.ProductID
+JOIN Production.ProductSubcategory C
+ON B.ProductSubcategoryID = C.ProductSubcategoryID
+JOIN Production.ProductCategory D
+ON C.ProductCategoryID = D.ProductCategoryID
+)A 
+
+PIVOT(
+    SUM(LineTotal)
+    FOR ProductCategoryName IN ([Bikes], [clothing], [Accessories], [Components])
+) B;
+
+-- Another way of pivot
+SELECT
+[Order Quantity]= OrderQty,
+[Bikes],
+[clothing]
+FROM
+
+(SELECT
+ProductCategoryName = D.NAME, 
+A.LineTotal,
+A.OrderQty
+
+FROM Sales.SalesOrderDetail A
+JOIN Production.Product B 
+ON A.ProductID =B.ProductID
+JOIN Production.ProductSubcategory C
+ON B.ProductSubcategoryID = C.ProductSubcategoryID
+JOIN Production.ProductCategory D
+ON C.ProductCategoryID = D.ProductCategoryID
+)A 
+
+PIVOT(
+    SUM(LineTotal)
+    FOR ProductCategoryName IN ([Bikes], [clothing], [Accessories], [Components])
+) B
+
+ORDER BY 1;
+
+-- FOR XML PATH With STUFF - Exercises
+-- Exercise 1
+SELECT 
+[Sales Representative], [Buyer], [Janitor]
+FROM HumanResources.Employee
+
+PIVOT(
+    AVG(VacationHours)
+    FOR JobTitle IN ([Sales Representatives], [Buyers], [Janitors])
+)
+ORDER BY 1;
